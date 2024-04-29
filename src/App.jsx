a40db47d37ad9f7d799022de9d1c650e13487d37
Fanni Wihl <fanniwihl@gmail.com>
2024-04-29 12:46:26 +0200

    adding basename to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 import AboutPage from "./pages/AboutPage";
 import Home from "./pages/Home";
@@ -10,19 +10,42 @@ import PhotographyPage from "./pages/PhotographyPage";
 import WebdesignPage from "./pages/WebdesignPage";
 import ContactPage from "./pages/ContactPage";
 
-
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/">
         <Routes>
-          <Route path='/' element={<Home />} errorelement={<ErrorPage />} />
-          <Route path='/about' element={<AboutPage />} errorelement={<ErrorPage />} />
-          <Route path='/works/social-media' element={<SocialMediaPage />} errorelement={<ErrorPage />} />
-          <Route path='/works/advertisement' element={<AdsPage />} errorelement={<ErrorPage />} />
-          <Route path='/works/photography' element={<PhotographyPage />} errorelement={<ErrorPage />} />
-          <Route path='/works/webdesign' element={<WebdesignPage />} errorelement={<ErrorPage />} />
-          <Route path='/contact' element={<ContactPage />} errorelement={<ErrorPage />} />
+          <Route path="/" element={<Home />} errorelement={<ErrorPage />} />
+          <Route
+            path="/about"
+            element={<AboutPage />}
+            errorelement={<ErrorPage />}
+          />
+          <Route
+            path="/works/social-media"
+            element={<SocialMediaPage />}
+            errorelement={<ErrorPage />}
+          />
+          <Route
+            path="/works/advertisement"
+            element={<AdsPage />}
+            errorelement={<ErrorPage />}
+          />
+          <Route
+            path="/works/photography"
+            element={<PhotographyPage />}
+            errorelement={<ErrorPage />}
+          />
+          <Route
+            path="/works/webdesign"
+            element={<WebdesignPage />}
+            errorelement={<ErrorPage />}
+          />
+          <Route
+            path="/contact"
+            element={<ContactPage />}
+            errorelement={<ErrorPage />}
+          />
         </Routes>
       </Router>
     </>

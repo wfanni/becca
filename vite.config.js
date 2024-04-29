bc850795023a765737240eb4f45be52aa94caf9d
Fanni Wihl <fanniwihl@gmail.com>
2024-04-29 13:21:24 +0200

    gh updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/becca',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/',
   build: {
     outDir: 'build'
   }
